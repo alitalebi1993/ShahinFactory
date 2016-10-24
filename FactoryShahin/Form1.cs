3c9dbbd04d0baf9fce9642d1b9b94900789ce72a
alitaleb1993 <a.talebi1993@gmail.com>
2016-10-24 23:19:55 +0330

    alitest

--- FactoryShahin/Form1.cs
+++ FactoryShahin/Form1.cs
@@ -7,7 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using ShahinDataBaseConfigue;
+using ShahinShoese;
 namespace FactoryShahin
 {
     public partial class Form1 : Form
@@ -15,6 +16,20 @@ namespace FactoryShahin
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ShahinFactoryDb db = new ShahinFactoryDb();
+                db.Auction.ToList();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
