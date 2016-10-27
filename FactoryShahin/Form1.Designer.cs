57471f7fe5e410c2036df12d6199dbda65b8fb0e
alitaleb1993 <a.talebi1993@gmail.com>
2016-10-28 01:20:22 +0330

    editmainform

--- FactoryShahin/Form1.Designer.cs
+++ FactoryShahin/Form1.Designer.cs
@@ -29,61 +29,115 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.اطلاعاتپایهToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تعریفسایزToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تعریفانبارToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تعریفواحدمحصولToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تعریفنوعمحصولToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تعریفنوعمشترToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.تعریفمشتریToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(124, 49);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(104, 217);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
+            this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // menuStrip1
             // 
-            this.button2.Location = new System.Drawing.Point(124, 85);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.اطلاعاتپایهToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
             // 
-            // button3
+            // اطلاعاتپایهToolStripMenuItem
             // 
-            this.button3.Location = new System.Drawing.Point(124, 121);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 28);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.اطلاعاتپایهToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.تعریفسایزToolStripMenuItem,
+            this.تعریفانبارToolStripMenuItem,
+            this.تعریفواحدمحصولToolStripMenuItem,
+            this.تعریفنوعمحصولToolStripMenuItem,
+            this.تعریفنوعمشترToolStripMenuItem,
+            this.تعریفمشتریToolStripMenuItem});
+            this.اطلاعاتپایهToolStripMenuItem.Name = "اطلاعاتپایهToolStripMenuItem";
+            this.اطلاعاتپایهToolStripMenuItem.Size = new System.Drawing.Size(80, 20);
+            this.اطلاعاتپایهToolStripMenuItem.Text = "اطلاعات پایه";
+            // 
+            // تعریفسایزToolStripMenuItem
+            // 
+            this.تعریفسایزToolStripMenuItem.Name = "تعریفسایزToolStripMenuItem";
+            this.تعریفسایزToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.تعریفسایزToolStripMenuItem.Text = "تعریف سایز";
+            // 
+            // تعریفانبارToolStripMenuItem
+            // 
+            this.تعریفانبارToolStripMenuItem.Name = "تعریفانبارToolStripMenuItem";
+            this.تعریفانبارToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.تعریفانبارToolStripMenuItem.Text = "تعریف انبار";
+            // 
+            // تعریفواحدمحصولToolStripMenuItem
+            // 
+            this.تعریفواحدمحصولToolStripMenuItem.Name = "تعریفواحدمحصولToolStripMenuItem";
+            this.تعریفواحدمحصولToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.تعریفواحدمحصولToolStripMenuItem.Text = "تعریف واحد محصول";
+            // 
+            // تعریفنوعمحصولToolStripMenuItem
+            // 
+            this.تعریفنوعمحصولToolStripMenuItem.Name = "تعریفنوعمحصولToolStripMenuItem";
+            this.تعریفنوعمحصولToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.تعریفنوعمحصولToolStripMenuItem.Text = "تعریف نوع محصول";
+            // 
+            // تعریفنوعمشترToolStripMenuItem
+            // 
+            this.تعریفنوعمشترToolStripMenuItem.Name = "تعریفنوعمشترToolStripMenuItem";
+            this.تعریفنوعمشترToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.تعریفنوعمشترToolStripMenuItem.Text = "تعریف نوع مشتری";
+            // 
+            // تعریفمشتریToolStripMenuItem
+            // 
+            this.تعریفمشتریToolStripMenuItem.Name = "تعریفمشتریToolStripMenuItem";
+            this.تعریفمشتریToolStripMenuItem.Size = new System.Drawing.Size(175, 22);
+            this.تعریفمشتریToolStripMenuItem.Text = "تعریف مشتری";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(379, 321);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
+            this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
             this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem اطلاعاتپایهToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تعریفسایزToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تعریفانبارToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تعریفواحدمحصولToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تعریفنوعمحصولToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تعریفنوعمشترToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem تعریفمشتریToolStripMenuItem;
     }
 }
 
